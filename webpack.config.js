7b197171c3ffa4b474d52e959bee1871fd83252c
Lespaulbasti91 <Lespaulbasti91@users.noreply.github.com>
2018-09-06 21:17:58 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+
+    entry: "./src/js/index.js",
+
+    output: {
+        // options related to how webpack emits results
+        path: path.resolve(__dirname, "public"), // string
+        // the target directory for all output files
+        // must be an absolute path (use the Node.js path module)
+        filename: "bundle.js", // string    // the filename template for entry chunks
+        publicPath: "./src/assets/", // string    // the url to the output directory resolved relative to the HTML page
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
