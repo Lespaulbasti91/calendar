7aed17cf5023036fa926795a85afca8dd83bb8e9
Lespaulbasti <Sebastian.Schieck91@gmail.com>
2018-09-30 21:08:39 +0200

    added modules for scss and css import

--- src/js/calendar/calendar.jsx
+++ src/js/calendar/calendar.jsx
@@ -5,32 +5,44 @@ class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      today: moment(),
-      week: moment().week(),
-      year: moment.months(),
+      today: moment('2018.12.28'),
     };
   }
 
   render() {
-    const format = 'DD.MM.YYYY';
-    const startday = moment('28.12.2018', format);
-    const startWeek = startday.startOf('month').week();
-    const endWeek = startday.endOf('month').week();
+    const { today } = this.state;
+    const format = 'YYYY.MM.DD';
+    const startday = moment(today, format);
+    const startWeek = startday.startOf('month').format('w YYYY');
 
+    const weeksInMonth = moment(moment(startday).endOf('month') - moment(startday).startOf('month')).weeks();
 
     const calendar = [];
-    for (let week = startWeek; week < endWeek; week++) {
+    for (let i = 0; i < weeksInMonth; i++) {
+      let week = moment(startWeek, 'w YYYY').add(i, 'week');
+      week = moment(week).format('w');
+
+      const startdayWeek = moment(week, 'w YYYY').weekday(1).format(format);
+
+      console.log(startdayWeek);
       calendar.push({
-        week,
-        days: Array(7).fill(0).map((n, i) => startday.week(week).startOf('week').clone()
-          .add(n + i, 'day')),
+        week: week,
+        days: Array(7).fill(0).map((n, j) => moment(startdayWeek, format).clone()
+          .add(j, 'day').format('YYYY-MM-DD')),
       });
     }
 
     console.log(calendar);
 
     return (
-      <div>Calendar</div>
+      <div>
+        {calendar.map((item, key) =>
+          <div key={key}>
+            {item.week}
+            {item.days.map((day, index) => <div key={index}>{day}</div>)}
+          </div>)}
+        <div></div>
+      </div>
     );
   }
 }
